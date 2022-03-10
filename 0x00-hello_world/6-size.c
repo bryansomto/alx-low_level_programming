ed6536a948669c48cee8df3558392995b24a6b1c
Bryan Somto <bryansomto@gmail.com>
2022-03-11 00:47:16 +0100

    removed trailing whitespace in task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-    char ch = ' ';
-    int int_num = 0;
-    int long_int = 0l;
-    int long_long_int = 0ll;
-    float float_num = 0.0f;
+	char ch = ' ';
+	int int_num = 0;
+	int long_int = 0l;
+	int long_long_int = 0ll;
+	float float_num = 0.0f;
 
-    printf("Size of a char: %d byte(s)\n", sizeof(char));
-    printf("Size of an int: %d byte(s)\n", sizeof(int));
-    printf("Size of a long int: %ld byte(s)\n", sizeof(long));
-    printf("Size of a long long int: %lld byte(s)\n", sizeof(long long));
-    printf("Size of a float: %d byte(s)\n", sizeof(float));
-    return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
