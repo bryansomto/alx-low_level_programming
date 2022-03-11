20aba1c1fe3e940f3a82924841e6ff7546295f35
Bryan Somto <bryansomto@gmail.com>
2022-03-12 00:38:07 +0100

    fixed spaces: task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -14,19 +13,19 @@
 
 int main(void)
 {
-        int n;
+	int n;
 	int m;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	m = n % 10;
 
-        if (m > 5)
+	if (m > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, m);
 	else if (m == 0)
 		printf("Last digit of %i is %i and is 0\n", n, m);
 	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
-
-        return (0);
+	
+	return (0);
 }
