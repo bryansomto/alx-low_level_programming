fe756848a3d4e7df0ac3c53dee8dec808a6845ef
Bryan Somto <bryansomto@gmail.com>
2022-03-11 01:27:09 +0100

    added betty comment style for main and return

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - 
-			Dora Korpar, 2015-10-19");
+	fprintf(
+	stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	);
 	return (1);
 }
