a9dbb3c53b017a2f79d1c066953d1958ccf6d533
Bryan Somto <bryansomto@gmail.com>
2022-03-11 01:22:39 +0100

    added betty comment style for main and return

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main: Entry point
- * return: returns 1
+ * main - Entry point
+ *
+ * Return: return 1 (Error)
  */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - 
+			Dora Korpar, 2015-10-19");
 	return (1);
 }
