0e285083d0e6808827f08f9ce7f821cf658beea0
Bryan Somto <bryansomto@gmail.com>
2022-03-12 01:25:56 +0100

    betty format compliance

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: Print all letters of the alphabets using putchar()
- * 		letters are printed in lower and upper cases
+ *		letters are printed in lower and upper cases
  *
  * Return: program returns 0 (Success)
  */
@@ -21,7 +21,7 @@ int main(void)
 		putchar(ch);
 		ch++;
 	} while (ch <= 'z');
-	
+
 	do {
 		putchar(CH);
 		CH++;
