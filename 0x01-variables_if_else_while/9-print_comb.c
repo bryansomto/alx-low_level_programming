4641480ada1abf791f3fcb09d9242428d0719f96
Bryan Somto <bryansomto@gmail.com>
2022-03-12 02:28:59 +0100

    started task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: print all possible combination of a single digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		putchar(',');
+	}
+	return (0);
+}
