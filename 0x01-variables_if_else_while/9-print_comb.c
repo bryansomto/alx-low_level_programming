7b13d5fdc2cc29bec573af4637edda2ba170247d
Bryan Somto <bryansomto@gmail.com>
2022-03-12 02:42:58 +0100

    add if statement within loop

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,16 +14,13 @@ int main(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		if (x != 9)
+		putchar(x + '0');
+		if (x < 9)
 		{
-			putchar(x + '0');
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar(x + '0');
-		}
 	}
+	putchar('\n');
 	return (0);
 }
