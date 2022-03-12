54094155338e06c07692b497c4567720fa26529b
Bryan Somto <bryansomto@gmail.com>
2022-03-12 01:19:52 +0100

    begun task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all letters of the alphabets using putchar()
+ * 		letters are printed in lower and upper cases
+ *
+ * Return: program returns 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	CH = 'A';
+
+	do {
+		putchar(ch);
+		ch++;
+	} while (ch <= 'z');
+	
+	do {
+		putchar(CH);
+	} while (CH <= 'Z');
+
+	putchar('\n');
+
+	return (0);
+}
