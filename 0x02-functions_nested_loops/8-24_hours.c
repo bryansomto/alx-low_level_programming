cda2ec328ebe9e1a5d882fc4697a1b035d42dd89
Bryan Somto <bryansomto@gmail.com>
2022-03-16 01:15:27 +0100

    enhanced task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,16 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for (hr = 0; min < 60; min++)
+	for (hr = 0; hr < 24; hr++)
 	{
-		_putchar((hr / 10) + '0');
-		_putchar((hr % 10) + '0');
-		_putchar(':');
-		_putchar((min / 10) + '0');
-		_putchar((min % 10) + '0');
-		_putchar('\n');
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
