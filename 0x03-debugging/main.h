cc91a58cb9c219cf5ec5380cbbb189dc140f4254
Bryan Somto <bryansomto@gmail.com>
2022-03-16 21:35:49 +0100

    added header functions in task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int, int);
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
