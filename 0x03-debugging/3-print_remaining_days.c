0e7917164b8e8b55806d915b790e398b827d7066
Bryan Somto <bryansomto@gmail.com>
2022-03-17 21:45:16 +0100

    fixed task 3 in debugging project

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,8 +27,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Invalid date: %02d/%02d/%o4d\n", month, day - 31, year);
+			
 		}
 		else
 		{
