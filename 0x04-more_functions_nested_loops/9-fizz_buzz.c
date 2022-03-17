37c96df39b1d7323aec040cf2795691a456eec99
Bryan Somto <bryansomto@gmail.com>
2022-03-17 21:09:29 +0100

    added return statement in task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 != 0))
 			printf("Fizz ");
@@ -25,6 +25,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
+	printf("Buzz");
 	putchar('\n');
 	return (0);
 }
