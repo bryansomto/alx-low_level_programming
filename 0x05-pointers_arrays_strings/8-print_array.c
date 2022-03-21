312da642dda135b231d78b39b4e6a72adfa6d579
Bryan Somto <bryansomto@gmail.com>
2022-03-21 23:49:51 +0100

    added new line to last digit in task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,7 +16,9 @@ void print_array(int *a, int n)
 	{
 		if (i == (n - 1))
 			printf("%d\n", *(a + i));
-		else
+		else if (i <= (n - 1))
 			printf("%d, ", *(a + i));
+		else
+			printf('\n');
 	}
 }
