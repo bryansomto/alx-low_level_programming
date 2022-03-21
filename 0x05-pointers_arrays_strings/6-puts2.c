8e81521afc3c8c7881b8e4b091f2cf74982ef9f3
Bryan Somto <bryansomto@gmail.com>
2022-03-22 00:13:03 +0100

    made changes to for loop in task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != 0; i += 2)
-		_putchar(str[i]);
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
