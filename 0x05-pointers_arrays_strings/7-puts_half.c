9f4acb42cb40388accd8a03d6a51c8dfdcccbdc5
Bryan Somto <bryansomto@gmail.com>
2022-03-22 01:46:49 +0100

    additional changes to task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,21 +9,12 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; *(str + i) != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
-	if (i % 2 == 0)
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		for (i = i / 2; *(str + i) != '\0'; i++)
-		{
-			_putchar(*(str + i));
-		}
-	}
-	else
-	{
-		for (i = (i - 1) / 2; *(str + i) != '\0'; i++)
-		{
-			_putchar(*(str + (i + 1)));
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
