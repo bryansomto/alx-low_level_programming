d22f712282c1c5250e5287c183a1c7c3c15a0819
Bryan Somto <bryansomto@gmail.com>
2022-03-24 23:37:53 +0100

    used proper comment format in main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-// char *decode(char *str); /* practice file prototype */
+/* char *decode(char *str); practice file prototype */
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
