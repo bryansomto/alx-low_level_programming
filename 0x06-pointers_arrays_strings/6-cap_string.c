8aa7cfe4ff218ccb35224fc6d5a3e57b89e6b322
Bryan Somto <bryansomto@gmail.com>
2022-03-24 23:47:17 +0100

    misc corrections

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
  *
  * Return: converted string
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
