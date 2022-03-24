bcc133a71d258640c314c721765add37f366117a
Bryan Somto <bryansomto@gmail.com>
2022-03-24 23:40:30 +0100

    misc corrections

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,25 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, n;
+	int i = 0, op = 0;
 
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		;
-	for (j = 0; *(s2 + j) != '\0'; j++)
-		;
-	if (i == j)
+	while (op == 0)
 	{
-		n = 0;
-		return (n);
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
 	}
-	else if (i > j)
-	{
-		n = 15;
-		return (n);
-	}
-	else
-	{
-		n = -15;
-		return (n);
-	}
+	return (op);
 }
