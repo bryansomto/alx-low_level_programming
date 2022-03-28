d12183c8e9ba92f6150906e44b504b2b1f9da4a4
Bryan Somto <bryansomto@gmail.com>
2022-03-29 00:20:35 +0100

    C - more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,12 +13,15 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j, count;
 
 	count = 0;
-	for (i = 0; *(accept + i) != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(s + j) != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*(accept + i) == *(s + j))
+			if (*(s + i) == *(accept + j))
+			{
 				count++;
+				break;
+			}
 		}
 	}
 	return (count);
