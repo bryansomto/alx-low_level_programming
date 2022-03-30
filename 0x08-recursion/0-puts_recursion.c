b9aa6fee22246e8b440272d29d096417ac586c3c
Bryan Somto <bryansomto@gmail.com>
2022-03-30 15:15:02 +0100

    C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,6 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s);
 		_putchar('\n');
 	}
 	else
@@ -17,4 +16,5 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	
 }
