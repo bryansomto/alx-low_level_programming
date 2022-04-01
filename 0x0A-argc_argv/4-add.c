c8e495bd72307a43d8c18e4b1a42cf5b12fc7668
Bryan Somto <bryansomto@gmail.com>
2022-04-02 00:40:34 +0100

    c - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,48 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-#include "main.h"
 
-/**
- * main - program entry
- * @s: argument count
- * @p: argument vector
- * Return: returns sum or 0 if arguments are not all integers
- */
-int _add(int s, char *p[])
-{
-	int i, sum;
-
-	sum = 0;
-	if (!(s > 1))
-		printf("%d\n", 0);
-	if (s > 1)
-	{
-		for (i = 1; i < s; i++)
-		{
-			if (isdigit(*p[i]))
-			{
-				sum += atoi(p[i]);
-				return (sum);
-			}
-			else
-				return (0);
-		}
-		printf("%d\n", sum);
-	}
-}
 /**
  * main - program entry
  * @argc: argument count
  * @argv: argument vector
  * Return: Always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int r;
-	char *p = &argv;
+	int i, sum;
 
-	r = _add(argc, *p);
-	printf("%d\n", r);
+	sum = 0;
+	if (!(argc > 1))
+		printf("%d\n", 0);
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (isdigit(*argv[i]))
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
 	return (0);
 }
