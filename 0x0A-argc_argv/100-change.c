478655f177d7428bebcc21009ac739a9a01b7bd7
Bryan Somto <bryansomto@gmail.com>
2022-04-01 21:59:39 +0100

    C - argc and argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,17 +16,19 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) == 10)
+	else if (atoi(argv[1]) > 0 && atoi(argv[1]) < 13)
 		printf("%d\n", 1);
-	else if (atoi(argv[1]) > 10 && atoi(argv[1]) <= 50)
-		printf("%d\n", 2);
-	else if (atoi(argv[1]) > 50 && atoi(argv[1]) <= 150)
+	else if (atoi(argv[1]) >= 13 && atoi(argv[1]) < 100)
+		printf("%d\n", 3);
+	else if (atoi(argv[1]) == 100)
+		printf("%d\n", 4);
+	else if (atoi(argv[1]) > 100 && atoi(argv[1]) <= 150)
 		printf("%d\n", 5);
 	else if (atoi(argv[1]) > 150 && atoi(argv[1]) <= 550)
 		printf("%d\n", 10);
 	else if (atoi(argv[1]) > 550 && atoi(argv[1]) <= 10000)
 		printf("%d\n", 25);
-	else if (atoi(argv[1]) < 10)
+	else if (atoi(argv[1]) < 0)
 		printf("%d\n", 0);
 	return (0);
 }
