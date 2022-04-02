6d93a6052144560bf2db23ada80a2376dee41029
Bryan Somto <bryansomto@gmail.com>
2022-04-02 01:21:18 +0100

    completed advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,22 +11,30 @@
 
 int main(int argc, char *argv[])
 {
-	if (!(argc > 1))
+	int cents, ncoins = 0;
+
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) == 10)
-		printf("%d\n", 1);
-	else if (atoi(argv[1]) == 100)
-		printf("%d\n", 4);
-	else if (atoi(argv[1]) > 100 && atoi(argv[1]) <= 150)
-		printf("%d\n", 5);
-	else if (atoi(argv[1]) > 150 && atoi(argv[1]) <= 550)
-		printf("%d\n", 10);
-	else if (atoi(argv[1]) > 550 && atoi(argv[1]) <= 10000)
-		printf("%d\n", 25);
-	else if (atoi(argv[1]) < 10)
-		printf("%d\n", 0);
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
 	return (0);
 }
