8e9f81916e17c8eb850366a03cfa20e161e7a73d
Bryan Somto <bryansomto@gmail.com>
2022-04-05 04:54:33 +0100

    C - malloc, free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ *
+ * @str: pointer to string
+ *
+ * Return: char type
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *p;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	p = (char *)malloc(sizeof(char) * (i + 1));
+	if (str == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+		p[j] = str[j];
+	return (p);
+	free(p);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ *
+ * @width: array width
+ * @height: array height
+ *
+ * Return: returns NULL if param is -ve and NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = malloc(sizeof(int) * height);
+	if (p == NULL)
+	{
+		return (NULL);
+		free(p);
+	}
+	for (i = 0; i < width; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(p[i]);
+			free(p);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+	return (p);
+	free(p);
+}
