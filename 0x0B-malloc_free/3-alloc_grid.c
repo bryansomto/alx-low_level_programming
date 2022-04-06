6ce0289c46f8ccb37639eda344188e8004157bfe
Bryan Somto <bryansomto@gmail.com>
2022-04-06 20:12:32 +0100

    updated task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,33 +11,40 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
+	int i, j, a, b;
 	int **p;
 
 	if (width <= 0 || height <= 0)
-		return (NULL);
-	p = malloc(sizeof(int) * (width + height));
-	if (p == NULL)
 	{
 		return (NULL);
-		free(p);
 	}
-	for (i = 0; i < width; i++)
+	else
 	{
-		p[i] = malloc(sizeof(int) * (width - 1));
-		if (p[i] == NULL)
+		p = (int **) malloc(height * sizeof(int *));
+		/* we have to make a malloc per pointer*/
+		if (!p)
 		{
-			for (i--; i >= 0; i--)
-				free(p[i]);
 			free(p);
 			return (NULL);
 		}
+		for (i = 0; i < height; i++)
+		{
+			p[i] = (int *) malloc(width * sizeof(int));
+			if (!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				free(p);
+				return (NULL);
+			}
+		}
+		for (a = 0; a < height; a++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				p[a][b] = 0;
+			}
+		}
+		return (p);
 	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-			p[i][j] = 0;
-	}
-	return (p);
-	free(p);
 }
