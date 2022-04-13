6c692c4c12f306222eaa602a8910a24110fd0f73
Bryan Somto <bryansomto@gmail.com>
2022-04-13 21:19:42 +0100

    task 2 commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,5 +7,9 @@ void print_name(char *name, void (*f)(char *));
 void print_elem(int elem);
 void print_elem_hex(int elem);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
+int is_98(int elem);
 
 #endif /* FUNCTION_POINTERS_H */
