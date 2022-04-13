00a65eb696d529ea13dee46e1021681cccae9a41
Bryan Somto <bryansomto@gmail.com>
2022-04-13 20:34:05 +0100

    removed trailing whitespace error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a 
+ * array_iterator - executes a function given as a
  * parameter on each element of an array
  *
  * @array: pointer to array fo functions
