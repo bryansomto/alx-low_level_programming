17de3bd4102b6a68cafeec3dfaac60bfb4db23cf
Bryan Somto <bryansomto@gmail.com>
2022-04-13 21:59:17 +0100

    added task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "function_pointers.h"
+/**
+ * op_add - addition operation
+ * @a: integer a
+ * @b: integer b
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction operation
+ * @a: integer a
+ * @b: integer b
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - subtraction operation
+ * @a: integer a
+ * @b: integer b
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division operation
+ * @a: integer a
+ * @b: integer b
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - subtraction operation
+ * @a: integer a
+ * @b: integer b
+ * Return: modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
