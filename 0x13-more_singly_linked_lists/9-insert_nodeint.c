983d0884bd286137223ce8645b75b68fc75d2603
Bryan Somto <bryansomto@gmail.com>
2022-04-26 22:14:03 +0100

    fixed betty's trailing whitespace error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		for (i = 0; i < (idx - 1) && temp != NULL; i++)
 			temp = temp->next;
 	}
-	
+
 	if (idx == 0)
 	{
 		new->next = *head;
