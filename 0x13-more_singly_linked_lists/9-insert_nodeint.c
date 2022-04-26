4478c73b7bc6ca8d200f09849cafc991b2bdc2fa
Bryan Somto <bryansomto@gmail.com>
2022-04-26 22:18:46 +0100

    modified task 9 to meet checker requirements

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 	}
 
+	if (temp == NULL && idx != 0)
+		return (NULL);
+
 	if (idx == 0)
 	{
 		new->next = *head;
