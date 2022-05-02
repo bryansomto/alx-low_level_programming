8a2a5011dcdf150be353fef67aa1fa008dcadb26
Bryan Somto <bryansomto@gmail.com>
2022-05-02 16:52:50 +0100

    modified memory allocation

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(letters));
+	buf = malloc(sizeof(char) * letters);
+
 	if (buf == NULL)
 		return (0);
 
