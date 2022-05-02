3c8bc310660d218a0fe375f869add7751c50f5ce
Bryan Somto <bryansomto@gmail.com>
2022-05-02 17:39:10 +0100

    removed unused variable

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,13 +6,12 @@
  * @filename: name of file to be created
  * @text_content: a NULL terminated string to write to the file
  *
- * Return: actual number of letters it reads and print
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
 	int i, fd, txt;
-	char *buf;
 
 	if (filename == NULL)
 		return (-1);
@@ -25,10 +24,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	for (i = 1; text_content[i] != '\0'; i++)
+	for (i = 0; text_content[i]; i++)
 			;
 
 	txt = write(fd, text_content, i);
+
+	if (text == -1)
+		return (-1);
 
 	close(fd);
 
