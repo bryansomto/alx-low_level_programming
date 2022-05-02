d802aa8e1b34e9b2c3460b4aa829473139189bb9
Bryan Somto <bryansomto@gmail.com>
2022-05-02 17:35:53 +0100

    modifications to fix compilation errors made

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,35 +11,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i, fd;
-	char *buf, *txt;
+	int i, fd, txt;
+	char *buf;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (i = 1; text_content[i] != '\0'; i++)
+	if (text_content == NULL)
+		text_content = "";
+
+	for (i = 1; text_content[i] != '\0'; i++)
 			;
 
-		buf = malloc(sizeof(char) * (i + 1));
+	txt = write(fd, text_content, i);
 
-		if (buf == NULL)
-			return (-1);
-
-		txt = write(fd, text_content, buf);
-	}
-	else
-	{
-		txt = write(fd, "", 1);
-	}
-	
 	close(fd);
-	free(buf);
+
 	return (1);
 }
